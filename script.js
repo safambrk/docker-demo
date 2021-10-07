5763ff96f63911dd522b567564d10fb663092c0e
safambrk <safaaitmbarek@gmail.com>
2021-10-07 23:28:55 +0100

    add mysql

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
+// MySQL 
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "1234",
+  database: "docker-demo"
+});
+
+connection.connect(error => {
+  if (error) throw error;
+  console.log("Successfully connected to the database.");
+});
+
+connection.end();
+
+// Express Get API
 const express = require('express')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send("Hello! This is my fisrt Docker Image! 😎")
+  res.json({ message : "Hello! This is my fisrt Docker Demo! 😎"})
 })
 
 app.listen(port, () => {
