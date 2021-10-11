86819e495f1b994e542555e78362429ebcef728d
safambrk <safaaitmbarek@gmail.com>
2021-10-11 15:05:36 +0100

    add docker compose

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
 // MySQL 
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: "localhost",
+  port: "3307",
   user: "root",
-  password: "1234",
+  password: "root",
   database: "docker-demo"
 });
 
-connection.connect(error => {
-  if (error) throw error;
-  console.log("Successfully connected to the database.");
+let data = null;
+
+pool.query('select id from user', (err, rows) => {
+  if(err) throw err;
+  console.log("Successfully connected to MySQL database!");
+  console.log("Data : ", rows);
+  data = rows;
 });
 
-connection.end();
 
-// Express Get API
+// Express API
 const express = require('express')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.json({ message : "Hello! This is my fisrt Docker Demo! 😎"})
+  res.json({ message : "Hello! This is my fisrt Docker Demo! 😎", data: data})
 })
 
 app.listen(port, () => {
